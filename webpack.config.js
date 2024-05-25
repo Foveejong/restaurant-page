95b645e5e49cc1c56e6ed392d49c22cb0ae9e5c7
foveejong <jovefong62@gmail.com>
2024-05-25 09:04:44 +0800

    Implement the main template for the pages
    Add new fonts and background image.

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,17 @@ module.exports = {
             inject: "head",
             scriptLoading: "defer"
         }),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
 }
