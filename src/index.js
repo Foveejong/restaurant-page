aa3d222a49dd0ded2eeab44fd97edbe6326f4a7c
foveejong <jovefong62@gmail.com>
2024-05-26 21:49:23 +0800

    Create feedback.js to create feedback page with 3 comments by 3 different users

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import "./styles.css";
 import { createHomePage } from "./home.js";
 import { createMenu } from "./menu.js";
+import { createFeedbackPage } from "./feedback.js";
 
 const content = document.querySelector("#content");
 // const homepage = createHomePage();
 // content.appendChild(homepage);
 
-const menu = createMenu();
+// const menu = createMenu();
+// content.appendChild(menu);
 
-content.appendChild(menu);
+const feedback = createFeedbackPage();
+content.appendChild(feedback);
